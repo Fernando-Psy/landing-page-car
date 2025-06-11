a940f2822eee6b775b2d932c6971407e24ba85b0
Fernando-Psy <fcesarjunior@gmail.com>
2025-06-10 23:02:09 -0300

    Modificando projeto

--- gulpfile.js
+++ gulpfile.js
@@ -1,79 +1,112 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const cleanCSS = require('gulp-clean-css');
-const autoprefixer = require('gulp-autoprefixer');
-const htmlmin = require('gulp-htmlmin');
+const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
+const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
+const rename = require('gulp-rename');
+const del = require('del');
 
-// Compilar SCSS
-function styles() {
-  return gulp
-    .src('./src/scss/**/*.scss')
-    .pipe(sass({ outputStyle: 'compressed' }))
-    .pipe(autoprefixer({
-      overrideBrowserslist: ['last 2 versions'],
-      cascade: false
-    }))
-    .pipe(cleanCSS())
-    .pipe(gulp.dest('./dist/css'));
+// Caminhos
+const paths = {
+    html: {
+        src: 'src/**/*.html',
+        dest: 'dist/'
+    },
+    styles: {
+        src: 'src/scss/**/*.scss',
+        dest: 'dist/css/'
+    },
+    scripts: {
+        src: 'src/js/**/*.js',
+        dest: 'dist/js/'
+    },
+    images: {
+        src: 'src/images/**/*',
+        dest: 'dist/images/'
+    }
+};
+
+// Limpar a pasta dist
+function clean() {
+    return del(['dist']);
 }
 
-// Minificar HTML
+// Copiar e minificar HTML
 function html() {
-  return gulp
-    .src('./src/*.html')
-    .pipe(htmlmin({
-      collapseWhitespace: true,
-      removeComments: true,
-      minifyCSS: true,
-      minifyJS: true
-    }))
-    .pipe(gulp.dest('./dist'));
+    return gulp.src(paths.html.src)
+        .pipe(gulp.dest(paths.html.dest))
+        .pipe(browserSync.stream());
 }
 
-// Minificar JavaScript
+// Compilar Sass, adicionar prefixos e minificar CSS
+function styles() {
+    return gulp.src('src/scss/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        .pipe(cleanCSS())
+        .pipe(rename({
+            basename: 'styles',
+            suffix: '.min'
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(paths.styles.dest))
+        .pipe(browserSync.stream());
+}
+
+// Concatenar e minificar JS
 function scripts() {
-  return gulp
-    .src('./src/js/**/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('./dist/js'));
+    return gulp.src(paths.scripts.src)
+        .pipe(sourcemaps.init())
+        .pipe(concat('main.js'))
+        .pipe(uglify())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(paths.scripts.dest))
+        .pipe(browserSync.stream());
 }
 
 // Otimizar imagens
 function images() {
-  return gulp
-    .src('./src/images/**/*')
-    .pipe(imagemin([
-      imagemin.gifsicle({interlaced: true}),
-      imagemin.mozjpeg({quality: 80, progressive: true}),
-      imagemin.optipng({optimizationLevel: 5}),
-      imagemin.svgo({
-        plugins: [
-          {removeViewBox: true},
-          {cleanupIDs: false}
-        ]
-      })
-    ]))
-    .pipe(gulp.dest('./dist/images'));
+    return gulp.src(paths.images.src)
+        .pipe(imagemin())
+        .pipe(gulp.dest(paths.images.dest))
+        .pipe(browserSync.stream());
 }
 
-// Watch files
-function watchFiles() {
-  gulp.watch('./src/scss/**/*.scss', styles);
-  gulp.watch('./src/*.html', html);
-  gulp.watch('./src/js/**/*.js', scripts);
-  gulp.watch('./src/images/**/*', images);
+// Servidor local com BrowserSync
+function serve() {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        },
+        port: 3000
+    });
+
+    gulp.watch(paths.styles.src, styles);
+    gulp.watch(paths.scripts.src, scripts);
+    gulp.watch(paths.html.src, html);
+    gulp.watch(paths.images.src, images);
 }
 
-// Build completo
-const build = gulp.parallel(styles, html, scripts, images);
+// Tasks compostas
+const build = gulp.series(clean, gulp.parallel(html, styles, scripts, images));
+const watch = gulp.series(build, serve);
 
-// Exports
-exports.styles = styles;
+// Exportar tasks
+exports.clean = clean;
 exports.html = html;
+exports.styles = styles;
 exports.scripts = scripts;
 exports.images = images;
-exports.watch = watchFiles;
 exports.build = build;
-exports.default = build;
+exports.watch = watch;
+exports.default = watch;
